480e349406685c889ab5bf09b3e97f2df60c9d5f
Syed Shameem <shahidclt93@gmail.com>
2024-07-12 16:53:09 +0530

    new fix

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -48,12 +48,9 @@ const Portfolio = () => {
             <h3 className="portfolio__title">Blog App</h3>
             <p className="portfolio__description">
               NexusInfo-BlogApp is a React JS website developed for users to
-              create, share, and manage their own blogs effortlessly. Built with
-              modern web technologies, this application provides a user-friendly
-              interface for both bloggers and readers alike. The application
-              also includes functionality to save blog posts locally using
-              localStorage, ensuring that users can access their drafts even
-              when offline.
+              create, share, and manage their own blogs effortlessly. The
+              application also includes functionality to save blog posts locally
+              using localStorage.
             </p>
             <p className="portfolio__tech-stack">
               React JS | Context Api | CSS
