6891dfb86cd9bbe8f55b233f364473fae0270c45
Syed Shameem <shahidclt93@gmail.com>
2024-08-14 15:00:48 +0530

    new feature added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import {BrowserRouter} from "react-router-dom"
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Analytics/>
     <App />
   </React.StrictMode>
 );
